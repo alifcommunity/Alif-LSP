55222f3343452abcc83ce856242f481db7c50acc
Omar Aglan <omar.aglan91@yahoo.com>
2025-08-19 02:40:36 +0300

    ميزة: تطبيق التسجيل المتوافق مع بروتوكول LSP عبر `window/logMessage`

--- src/include/Server.h
+++ src/include/Server.h
@@ -11,6 +11,9 @@ public:
 
 private:
 	void sendResponse(const json& response);
+	void sendNotification(const json& notification);
+	// 1: Error, 2: Warning, 3: Info, 4: Log
+	void logMessage(const std::string& message, int type = 3);
 	void initialize(const json& params);
 	void handleCompletion(const json& params, const json& id);
 };
